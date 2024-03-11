2916966fa9103a228779ebde287976193c3c4b0c
AruzhanIssabekova <150043321+AruzhanIssabekova@users.noreply.github.com>
2024-03-11 21:29:58 +0600

    Add files via upload

---
+++ HW37.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+chrome_path = "C:\Program Files (x86)\Google\Chrome\Application"
+
+chrome_o = Options()
+chrome_o.add_argument("--headless")
+
+s = Service(chrome_path)
+s.start()
+driver = webdriver.Chrome(service=s, options=chrome_o)
+
+driver.get("https://kurs1.kz/")
+
+
+time.sleep(5)
+
+
+usd_e = driver.find_element(By.XPATH, "//td[text()='USD']/following-sibling::td")
+eur_e = driver.find_element(By.XPATH, "//td[text()='EUR']/following-sibling::td")
+
+
+usd = usd_e.text
+eur = eur_e.text
+
+print("Курс доллара:", usd)
+print("Курс евро:", eur)
+
+
+driver.quit()
